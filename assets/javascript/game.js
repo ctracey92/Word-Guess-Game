9dcc882a577d4d479032c5ef8307d536ee2af4a6
Cody <cody.tracy92@gmail.com>
2019-02-09 08:59:10 -0600

    fixed the userGuess variable, fixed the logic that was crashing the browser

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
+//varaibles for wins/losses
+var wins = 0
+var losses = 0
+
+//variable for chances left
+var guessesLeft = 10
 
 //Array of words to guess
 var secretCodes = ["hacker", "blackhat","encrypt", "firewall", "password"]
 
 //Randomly select word (needs to be on game start)
-var word = secretCodes[Math.floor(Math.random()*secretCodes.length)]
-
-//creates the onkeyup event and logs it (currently is is only local and not global)
-
-var userInput = document.onkeyup = function(event) {
-    var userGuess = event.key.toLocaleLowerCase;
-    }
+    var word = secretCodes[Math.floor(Math.random()*secretCodes.length)]
+    console.log (word);  
 
 //Creates the place holder for unguessed letters
 var answerBox = [];
@@ -17,52 +18,64 @@ for (var i = 0; i < word.length; i++) {
 answerBox[i] = "_";
 }
 var remainingLetters = word.length; 
+console.log (answerBox);
+ // HERE IS WHERE THE GAME CODE GOES
+
 
     
-// HERE IS WHERE THE GAME CODE GOES
 
-while (remainingLetters > 0){
+    //creates the onkeyup event and logs it (currently is is only local and not global)
+
+    var userInput = document.onkeyup = function(event) {
+        console.log(event.key) ;
+        }
+
+
     //creates an array to store user guesses
     var userGuesses = [];
     document.onkeyup = function (e) {
-        var keyPress;
-        if (typeof event !== 'undefined') {
-            keyPress = event.keyCode;
-        }
-        else if (e) {
-            keyPress = e.which;
-        }
-        userGuesses(String.fromCharCode(keyPress));
-        return false;
+        var keyPress = e.key;
+        // if (typeof event !== 'undefined') {
+        //     keyPress = event.keyCode;
+        // }
+        // else if (e) {
+        //     keyPress = e.which;
+        // }
+        userGuesses.push(keyPress);
+        console.log(userGuesses);
     };
-    if ( userInput !== userGuesses) {
-        for (var answerUpdate = 0; answerUpdate < word.length; answerUpdate++){
-            if (word[answerUpdate] === userInput){
-                answerBox[answerUpdate] = userInput;
-                remainingLetters--;
-            }
+
+    // console.log(userGuesses);
+    
+    for (var answerUpdate = 0; answerUpdate < word.length; answerUpdate++) {
+        //If the user gets a letter right it prints the letter
+        if (word[answerUpdate] === userInput) {
+            answerBox[answerUpdate] = userInput;
+            remainingLetters--;
+        }
+        
+        //If the user is wrong subtract one guess
+        else {
+            guessesLeft--;
         }
     }
 
-    
-    
+    indexOf
+   
 
-    
-}
-//PSEUDO CODE
+// //PSEUDO CODE
 
 
+// // if (x === letter in word)
+// // print the letter
 
-// if (x === letter in word)
-// print the letter
+// // if (x !== letter in word && has not been used){
+// //     nchances - 1
+// // }
 
-// if (x !== letter in word && has not been used){
-//     nchances - 1
-// }
-
-// if (remainingLetters === 0) {
-//     print "You're in!!!"
-// }
+// // if (remainingLetters === 0) {
+// //     print "You're in!!!"
+// // }
 
 
 
